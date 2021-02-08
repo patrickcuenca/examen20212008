8523290ef513026c05c70acc36c23fef9b8c5d3e
patrickcuenca <patrickcuenca@ginebro.cat>
2021-02-08 20:06:42 +0100

    Primer commit activitat 4!

---
+++ js/seguentCuencaPatrick.js
@@ -0,0 +1,24 @@
+function passaSeguentPCL(objRebut){
+    let idObjPregAct = objRebut.parentElement.parentElement.id;
+    let colArticles = document.getElementsByTagName("article");
+    let idObjPregSeg;
+      for (let i = 0; i < colArticles.length; i++) {
+      if(colArticles[i].id == idObjPregAct){
+        idObjPregSeg = colArticles[i+1].id;
+        break;
+      };
+    }
+    amagaElementPCL(idObjPregAct);
+    mostraElementPCL(idObjPregSeg);
+  }
+
+
+  function mostraElementPCL(idRebut){
+    document.getElementById(idRebut).classList.add("elementVisiblePCL");
+    document.getElementById(idRebut).classList.remove("elementOcultPCL");
+  }
+
+  function amagaElementPCL(idRebut){
+    document.getElementById(idRebut).classList.remove("elementVisiblePCL");
+    document.getElementById(idRebut).classList.add("elementOcultPCL");
+  }
